0369551b096b0467cc627d869a15c3ee744145d8
Bliz33333 <32490678+Bliz33333@users.noreply.github.com>
2017-12-04 12:14:56 -0600

    basic work

--- src/Senior-IA-Driver.cpp
+++ src/Senior-IA-Driver.cpp
@@ -3,11 +3,11 @@
 #include "MeaningUnit.h"
 #include "PoeticDevice.h"
 #include "Translator.h"
-#include "GUI.h"
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <vector>
+#include <windows.h>
 
 bool startupLit();
 bool startupStages();
@@ -16,23 +16,82 @@ std::vector<PoeticDevice> readInDevices(std::string);
 
 std::map<std::string, class LitUnit> IBmap;
 std::map<std::string, class LitUnit> APmap;
+const char g_szClassName[] = "myWindowClass";
 
-int main()
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	if (!startupLit())
+	switch (msg)
 	{
-		return -3;
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
-
-	if (!startupStages())
-	{
-		return -2;
-	}
-
-	WinMain(0,0,0,0);
-	//std::cout << IBmap.find("Heroides1")->second.intoString();
-
 	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine, int nCmdShow)
+{
+	startupLit();
+	startupStages();
+
+	std::cout << IBmap.find("Heroides1")->second.translator.text.at(57);
+
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+
+	//Step 1: Registering the Window Class
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+		MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	// Step 2: Creating the Window
+	hwnd = CreateWindowEx(
+	WS_EX_CLIENTEDGE, g_szClassName, "The title of my window",
+	WS_OVERLAPPEDWINDOW,
+	CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+	NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+		MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// Step 3: The Message Loop
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
 }
 
 bool startupLit()			//todo make the error bool work
@@ -72,7 +131,8 @@ bool startupLit()			//todo make the error bool work
 		int numUnits = std::stoi(litline);
 
 		IBmap.insert(
-				{ title, LitUnit(readInDevices(title), author, meter, Translator(readInLines(meter, litfile, numUnits))) });
+				{ title, LitUnit(readInDevices(title), author, meter,
+						Translator(readInLines(meter, litfile, numUnits))) });
 	}
 	for (int i = 0; i < numAP; i++)
 	{
