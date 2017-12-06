9ee8ee54e07102c7ed19bbeccbc7d2ab8a4ad03d
Bliz33333 <32490678+Bliz33333@users.noreply.github.com>
2017-12-06 12:17:47 -0600

    work and procaelio

--- src/Senior-IA-Driver.cpp
+++ src/Senior-IA-Driver.cpp
@@ -1,18 +1,5 @@
-#include "Senior-IA-Driver.h"
-#include "LitUnit.h"
-#include "MeaningUnit.h"
-#include "PoeticDevice.h"
-#include "Translator.h"
-#include <iostream>
-#include <fstream>
-#include <map>
-#include <vector>
+#include "StartUpCommands.h"
 #include <windows.h>
-
-bool startupLit();
-bool startupStages();
-std::vector<MeaningUnit> readInLines(std::string, std::ifstream&, int);
-std::vector<PoeticDevice> readInDevices(std::string);
 
 std::map<std::string, class LitUnit> IBmap;
 std::map<std::string, class LitUnit> APmap;
@@ -38,10 +25,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		LPSTR lpCmdLine, int nCmdShow)
 {
-	startupLit();
+	startupLit(IBmap, APmap);
 	startupStages();
 
-	std::cout << IBmap.find("Heroides1")->second.translator.text.at(57);
+//	std::cout << "test"; //todo remove
+//	std::cout << IBmap.find("ProCaelio35-40")->second.translator.intoString();
+
+
 
 	WNDCLASSEX wc;
 	HWND hwnd;
@@ -70,9 +60,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 	// Step 2: Creating the Window
 	hwnd = CreateWindowEx(
-	WS_EX_CLIENTEDGE, g_szClassName, "The title of my window",
+	WS_EX_CLIENTEDGE, g_szClassName, "Latin Study Tool",
 	WS_OVERLAPPEDWINDOW,
-	CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+	CW_USEDEFAULT, CW_USEDEFAULT, 1000, 750,
 	NULL, NULL, hInstance, NULL);
 
 	if (hwnd == NULL)
@@ -81,6 +71,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		MB_ICONEXCLAMATION | MB_OK);
 		return 0;
 	}
+//
+//	HWND hwndButton = CreateWindow(
+//	    "BUTTON",  // Predefined class; Unicode assumed
+//	    "Go",      // Button text
+//	    WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles
+//	    10,         // x position
+//	    10,         // y position
+//	    100,        // Button width
+//	    100,        // Button height
+//	    hwnd,     // Parent window
+//	    NULL,       // No menu.
+//	    (HINSTANCE)GetWindowLong(hwnd, GWL_HINSTANCE),
+//	    NULL);
 
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
@@ -94,140 +97,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	return Msg.wParam;
 }
 
-bool startupLit()			//todo make the error bool work
-{
-	int numIB = 0;
-	int numAP = 0;
 
-	std::ifstream infile;
-	std::string line;
-
-	infile.open("data\\LitNames.txt", std::ios::out);
-
-	if (infile.is_open())
-	{
-		getline(infile, line);
-		numIB = std::stoi(line);
-		getline(infile, line);
-		numAP = std::stoi(line);
-	}
-
-	for (int i = 0; i < numIB; i++)
-	{
-		getline(infile, line);
-
-		std::ifstream litfile;
-		std::string litline;
-
-		std::string title = line;
-		litfile.open("data\\" + title + ".txt", std::ios::out);
-
-		getline(litfile, litline);
-		getline(litfile, litline);
-		std::string author = litline;
-		getline(litfile, litline);
-		std::string meter = litline;
-		getline(litfile, litline);
-		int numUnits = std::stoi(litline);
-
-		IBmap.insert(
-				{ title, LitUnit(readInDevices(title), author, meter,
-						Translator(readInLines(meter, litfile, numUnits))) });
-	}
-	for (int i = 0; i < numAP; i++)
-	{
-		getline(infile, line);
-
-		std::ifstream litfile;
-		std::string litline;
-
-		std::string title = line;
-		litfile.open("data\\" + title + ".txt", std::ios::out);
-
-		getline(litfile, litline);
-		getline(litfile, litline);
-		std::string author = litline;
-		getline(litfile, litline);
-		std::string meter = litline;
-		getline(litfile, litline);
-		int numUnits = std::stoi(litline);
-
-		APmap.insert(
-				{ title, LitUnit(readInDevices(title), author, meter,
-						Translator(readInLines(meter, litfile, numUnits))) });
-	}
-	return true;
-}
-
-bool startupStages()
-{
-	return true;
-}
-
-std::vector<MeaningUnit> readInLines(std::string meter, std::ifstream& litfile, //todo other meters
-		int numUnits)
-{
-	std::vector<MeaningUnit> units;
-	std::string litline;
-
-	getline(litfile, litline);
-	if (meter == "ElegiacCouplet")
-	{
-		std::string english;
-		std::string latin;
-		for (int i = 0; i < numUnits; i++)
-		{
-			english = "";
-			latin = "";
-
-			getline(litfile, litline);
-			latin += litline;
-			getline(litfile, litline);
-			latin += "\n" + litline;
-
-			getline(litfile, litline);
-			english += litline;
-			getline(litfile, litline);
-			english += +"\n" + litline;
-			getline(litfile, litline);
-			MeaningUnit m = MeaningUnit(latin, english);
-
-			units.push_back(m);
-		}
-
-	}
-	return units;
-}
-
-std::vector<PoeticDevice> readInDevices(std::string title)
-{
-	std::vector<PoeticDevice> p;
-
-	std::ifstream devfile;
-	std::string devline;
-
-	devfile.open("data\\" + title + "PD.txt", std::ios::out);
-
-	getline(devfile, devline);
-	int numDevices = std::stoi(devline);
-
-	for (int i = 0; i < numDevices; i++)
-	{
-		int count = 0;
-		std::string type = "";
-		std::string description = "";
-
-		getline(devfile, devline);
-		count = stoi(devline);
-
-		getline(devfile, devline);
-		type = devline;
-
-		getline(devfile, devline);
-		description = devline;
-
-		p.push_back(PoeticDevice(count, type, description));
-	}
-
-	return p;
-}
---
+++ src/StartUpCommands.cpp
@@ -0,0 +1,171 @@
+/*
+ * StartUpCommands.cpp
+ *
+ *  Created on: Dec 6, 2017
+ *      Author: s105308
+ */
+
+#include "StartUpCommands.h"
+
+bool startupStages() //todo finish
+{
+	return true;
+}
+
+bool startupLit(std::map<std::string, class LitUnit>& IBmap,
+		std::map<std::string, class LitUnit>& APmap)//todo make the error bool work
+{
+	int numIB = 0;
+	int numAP = 0;
+
+	std::ifstream infile;
+	std::string line;
+
+	infile.open("data\\LitNames.txt", std::ios::out);
+
+	if (infile.is_open())
+	{
+		getline(infile, line);
+		numIB = std::stoi(line);
+		getline(infile, line);
+		numAP = std::stoi(line);
+	}
+
+	for (int i = 0; i < numIB; i++)
+	{
+		getline(infile, line);
+
+		std::ifstream litfile;
+		std::string litline;
+
+		std::string title = line;
+		litfile.open("data\\" + title + ".txt", std::ios::out);
+
+		getline(litfile, litline);
+		getline(litfile, litline);
+		std::string author = litline;
+		getline(litfile, litline);
+		std::string meter = litline;
+		getline(litfile, litline);
+		int numUnits = std::stoi(litline);
+
+		IBmap.insert(
+				{ title, LitUnit(readInDevices(title), author, meter,
+						Translator(readInLines(meter, litfile, numUnits))) });
+	}
+	for (int i = 0; i < numAP; i++)
+	{
+		getline(infile, line);
+
+		std::ifstream litfile;
+		std::string litline;
+
+		std::string title = line;
+		litfile.open("data\\" + title + ".txt", std::ios::out);
+
+		getline(litfile, litline);
+		getline(litfile, litline);
+		std::string author = litline;
+		getline(litfile, litline);
+		std::string meter = litline;
+		getline(litfile, litline);
+		int numUnits = std::stoi(litline);
+
+		APmap.insert(
+				{ title, LitUnit(readInDevices(title), author, meter,
+						Translator(readInLines(meter, litfile, numUnits))) });
+	}
+	return true;
+}
+
+std::vector<MeaningUnit> readInLines(std::string meter, std::ifstream& litfile, //todo other meters
+		int numUnits)
+{
+	std::vector<MeaningUnit> units;
+	std::string litline;
+
+	getline(litfile, litline);
+	if (meter == "ElegiacCouplet")
+	{
+		std::string english;
+		std::string latin;
+		for (int i = 0; i < numUnits; i++)
+		{
+			english = "";
+			latin = "";
+
+			getline(litfile, litline);
+			latin += litline;
+			getline(litfile, litline);
+			latin += "\n" + litline;
+
+			getline(litfile, litline);
+			english += litline;
+			getline(litfile, litline);
+			english += +"\n" + litline;
+			getline(litfile, litline);
+			MeaningUnit m = MeaningUnit(latin, english);
+
+			units.push_back(m);
+		}
+
+	}
+
+	if (meter == "Prose")
+	{
+		std::string english;
+		std::string latin;
+		for (int i = 0; i < numUnits; i++)
+		{
+			english = "";
+			latin = "";
+
+			getline(litfile, litline);
+			latin += litline;
+
+			getline(litfile, litline);
+			english += litline;
+
+			getline(litfile, litline);
+			MeaningUnit m = MeaningUnit(latin, english);
+
+			units.push_back(m);
+		}
+
+	}
+	return units;
+}
+
+std::vector<PoeticDevice> readInDevices(std::string title)
+{
+	std::vector<PoeticDevice> p;
+
+	std::ifstream devfile;
+	std::string devline;
+
+	devfile.open("data\\" + title + "PD.txt", std::ios::out);
+
+	getline(devfile, devline);
+	int numDevices = std::stoi(devline);
+
+	for (int i = 0; i < numDevices; i++)
+	{
+		int count = 0;
+		std::string type = "";
+		std::string description = "";
+
+		getline(devfile, devline);
+		count = stoi(devline);
+
+		getline(devfile, devline);
+		type = devline;
+
+		getline(devfile, devline);
+		description = devline;
+
+		p.push_back(PoeticDevice(count, type, description));
+	}
+
+	return p;
+}
+
